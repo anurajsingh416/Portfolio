7824358028d236ac112bfc21272efa969af55289
Your Name <you@example.com>
2024-06-15 15:52:58 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About({theme}){
     return (
         <Element name="about" className="section">
             <div className={`px-12 h-screen flex flex-col md:flex-col lg:flex-row items-center justify-center ${theme? "bg-white text-gray":"bg-custom-gray text-white" }`}>
-                <img src="Anuraj_Profile_Pic.jpg" className="h-2/6 md:h-4/6 lg:h-4/6 mx-auto rounded-3xl hover:scale-95 transition-all" alt="Anuraj Singh's Profile Picture" />
+                <img src="Anuraj_Profile_Pic.jpg" className="h-2/6 md:h-4/6 lg:h-4/6 mx-auto rounded-3xl hover:-translate-y-1 transition-all" alt="Anuraj Singh's Profile Picture" />
                 <div className="flex flex-col items-center mx-auto ">
                     <h1 className="my-5 text-4xl font-bold">About Me</h1>
                     <hr className={`w-2/4 h-1  rounded border-0 ${theme?"bg-black":"bg-orange-600"}`} />
